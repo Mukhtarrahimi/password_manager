7fb9e11cd8a0de7493c84129dfb6878ce54a9a6d
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-21 09:50:21 +0430

    update encryption_key

--- password_manager.py
+++ password_manager.py
@@ -4,7 +4,8 @@ import secrets
 import string
 from cryptography.fernet import Fernet
 
-encryption_key = Fernet.generate_key()
+encryption_key = b"Ke3NG7IWYBOdv42RPxPRhdQcK0WRVY-cGnGvpHyTVvM="
+
 cipher_suite = Fernet(encryption_key)
 
 conn = sqlite3.connect("passwords.db")
