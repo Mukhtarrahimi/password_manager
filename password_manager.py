cec834cefc5ab081fba379d5617ca44489afc571
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-21 09:33:35 +0430

    detabase tabel create.

--- password_manager.py
+++ password_manager.py
@@ -9,3 +9,12 @@ cipher_suite = Fernet(encryption_key)
 
 conn = sqlite3.connect("passwords.db")
 c = conn.cursor()
+
+c.execute('''
+           CREATE TABLE IF NOT EXISTS passwords
+           (id INTEGER PRIMARY KEY AUTOINCREMENT,
+           website TEXT NOT NULL,
+           username TEXT NOT NULL,
+           password TEXT NOT NULL
+           )
+           ''')
