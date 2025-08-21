b6b93821de44f482937c14fd15cf70b0595cec4c
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-21 09:40:51 +0430

    while loop update & choice added.

--- password_manager.py
+++ password_manager.py
@@ -39,3 +39,21 @@ while True:
     print("\t4. View passwords")
     print("\t5. Delete password")
     print("\t6. Exit")
+
+    choice = input("Select an option: ")
+    if choice == "1":
+        register_user()
+    elif choice == "2":
+        change_password()
+    elif choice == "3":
+        add_password()
+    elif choice == "4":
+        view_passwords()
+    elif choice == "5":
+        delete_password()
+    elif choice == "6":
+        break
+    else:
+        print("Invalid choice! Please select again.")
+
+conn.close()
